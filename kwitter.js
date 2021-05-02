64102aaa0d61aa0468ccd38a9851b40f91f2a523
Sahiti-Kambham <78361096+Sahiti-Kambham@users.noreply.github.com>
2021-05-02 16:05:26 +0400

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,8 @@
+function btn_log() {
+
+    var user_name=document.getElementById("user_input").value;
+
+    localStorage.setItem("user_name",user_name);
+
+    window.location="kwitter_room.html";
+}
